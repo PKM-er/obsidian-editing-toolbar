e817d7e42cd31e5a8891d9d5fc2307f7899b24f9
MiserMagus <aa.rn.shn@gmail.com>
2025-12-01 18:04:55 +0000

    Update helper.ts

--- src/translations/helper.ts
+++ src/translations/helper.ts
@@ -8,6 +8,24 @@ import da from './locale/da';
 import de from './locale/de';
 import en from './locale/en';
 import enGB from './locale/en-gb';
+import es from './locale/es';
+import fr from './locale/fr';
+import hi from './locale/hi';
+import id from './locale/id';
+import it from './locale/it';
+import ja from './locale/ja';
+import ko from './locale/ko';
+import nl from './locale/nl';
+import no from './locale/no';
+import pl from './locale/pl';
+import pt from './locale/pt';
+import ptBR from './locale/pt-br';
+import ro from './locale/ro';
+import ru from './locale/ru';
+import tr from './locale/tr';
+import zhCN from './locale/zh-cn';
+import zhTW from './locale/zh-tw';
+
 const localeMap: { [k: string]: Partial<typeof en> } = {
   ar,
   cs: cz,
